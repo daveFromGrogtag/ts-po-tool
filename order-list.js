0e61bacc69286ec40f9f867f8e05c0b368f82acb
daveFromGrogtag <dave@grogtag.com>
2025-03-06 12:33:25 -0800

    added sort table button to order list

--- order-list.js
+++ order-list.js
@@ -7,12 +7,13 @@ function displayOrderList() {
 
     getDocs(allOrders)
         .then((docs) => {
-            let orderListElements = '<tr><th>Order Id</th><th>Order Status</th><th>QTY</th><th>Product</th><th>UPC</th><th>App Date</th><th>Due Date</th><th>Packing Slip</th></tr>'
+            let orderListElements = '<thead><tr><th>Order Id</th><th>Order Status</th><th>QTY</th><th>Product</th><th>UPC</th><th>App Date</th><th>Due Date</th><th>Packing Slip</th></tr></thead><tbody>'
             docs.forEach(order => {
               let rushStatus = order.data().rush ? " rush" : ""
                 let orderLink = `<tr class="${order.data().status}${rushStatus}"><td><a href="./order.html?id=${order.id}">${order.id}</a></td><td>${order.data().status}${rushStatus}</td><td>${order.data().totalQuantity}</td><td>${order.data().productType}</td><td>${order.data().upc?"UPC":"-"}</td><td>${order.data().approvalDate ? order.data().approvalDate : "-"}</td><td>${order.data().dueDate ? order.data().dueDate : "-"}</td><td><a href="./packing-list.html?id=${order.id}">Slip</a></td></tr>`
                 orderListElements += orderLink
             })
+            orderListElements + "</tbody>"
             orderList.innerHTML = orderListElements
         })
 }
