29eec8c3eed3349087f11d92ada5d722d5de5481
daveFromGrogtag <dave@grogtag.com>
2025-10-02 09:06:02 -0700

    restyled invoicer

--- invoicer.js
+++ invoicer.js
@@ -8,31 +8,16 @@ function markOrderAsInvoiced(orderId) {
         setDoc(doc(db, "orders", orderId), {
             status: orderStatus,
         },{merge: true}).then(() => {
+            alert(orderId + " marked as invoiced.")
             window.location.reload();
         })
     } catch (error) {
-        alert(`Order not saved`)
+        alert(`Order not marked`)
         console.error(error);
     }
 
 }
 
-function saveOrder(status) {
-    try {
-        let orderStatus = status
-        let orderId = document.getElementById("order-id-display").innerText
-        // let trackingNumber = document.getElementById('tracking-number').innerText ? document.getElementById('tracking-number').innerText : ''
-        setDoc(doc(db, "orders", orderId), {
-            status: orderStatus,
-            // tracking: trackingNumber
-        },{merge: true}).then(() => {
-            displayOrderInfo()
-        })
-    } catch (error) {
-        alert(`Order not saved`)
-        console.error(error);
-    }
-}
 
 function displayOrderList() {
     const orderList = document.getElementById("orderList")
@@ -43,7 +28,7 @@ function displayOrderList() {
             let orderListElements = '<thead><tr><th>Order Id</th><th>QTY</th><th>Product</th><th>Notes</th><th>UPC</th><th>App Date</th><th>Due Date</th><th>Tools</th></tr></thead><tbody>'
             docs.forEach(order => {
               let rushStatus = order.data().rush ? " rush" : ""
-                let orderLink = `<tr class="${order.data().status}${rushStatus}"><td><a href="./order.html?id=${order.id}">${order.id}</a></td><td>${order.data().totalQuantity}</td><td>${order.data().productType}</td><td>${order.data().orderNotes?order.data().orderNotes:""}</td><td>${order.data().upc?"UPC":"-"}</td><td>${order.data().approvalDate ? order.data().approvalDate : "-"}</td><td>${order.data().dueDate ? order.data().dueDate : "-"}</td><td><a href="./packing-list.html?id=${order.id}">Slip</a><button class="invoice-button" data-invoice-order-id="${order.id}" >Invoice</button></td></tr>`
+                let orderLink = `<tr class="${order.data().status}${rushStatus}"><td><a href="./order.html?id=${order.id}">${order.id}</a></td><td>${order.data().totalQuantity}</td><td>${order.data().productType}</td><td>${order.data().orderNotes?order.data().orderNotes:""}</td><td>${order.data().upc?"UPC":"-"}</td><td>${order.data().approvalDate ? order.data().approvalDate : "-"}</td><td>${order.data().dueDate ? order.data().dueDate : "-"}</td><td><button class="invoice-button" data-invoice-order-id="${order.id}" >Invoice</button></td></tr>`
                 orderListElements += orderLink
             })
             orderListElements + "</tbody>"
